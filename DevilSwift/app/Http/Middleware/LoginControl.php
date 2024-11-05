71923a468e817ccb9f5a283fdb8f3fbe509fb3ce
Jose <akstarlaantiguaestrellabeta@gmail.com>
2024-11-05 20:17:47 +0100

    pass

--- DevilSwift/app/Http/Middleware/LoginControl.php
+++ DevilSwift/app/Http/Middleware/LoginControl.php
@@ -14,11 +14,11 @@ class LoginControl
      *
      * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
      */
-    public function handle(Request $request, Closure $next): Response
+    public function handle(Request $request, Closure $pass): Response
     {
         if (Auth::check()) {
             return redirect('/');
         }
-        return $next($request);
+        return $pass($request);
     }
 }
