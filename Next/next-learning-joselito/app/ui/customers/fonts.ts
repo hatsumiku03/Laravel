cbdc20ba8868abb866805a41aa8bc2eb9a662220
Joselito <josalbarr@alu.edu.gva.es>
2024-12-17 10:40:27 +0100

    let's try

---
+++ Next/next-learning-joselito/app/ui/customers/fonts.ts
@@ -0,0 +1,8 @@
+export const lusitana = {
+    fontFamily: 'Lusitana',
+    fontStyle: 'normal',
+    fontWeight: 400,
+    src: `
+        url('https://fonts.googleapis.com/css2?family=Lusitana:wght@400&display=swap') format('woff2')
+    `,
+};
